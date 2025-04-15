63ffcf26de8094e7a58a866ff58bd39ce7262f32
DivityForge <divityforge@gmail.com>
2025-04-14 20:23:17 -0500

    Add local style update script before pull

--- scripts/style_update.py
+++ scripts/style_update.py
@@ -2,10 +2,13 @@
 with open('assets/css/style.scss', 'r') as file:
     css = file.read()
 
-# Example CSS modification (change font)
-css = css.replace('font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;', 'font-family: "Roboto", sans-serif;')
+# Replace old font with new font
+css = css.replace(
+    'font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;',
+    'font-family: "Roboto", sans-serif;'
+)
 
 with open('assets/css/style.scss', 'w') as file:
     file.write(css)
 
-print("CSS style updated successfully!")
+print("✅ CSS updated successfully!")
